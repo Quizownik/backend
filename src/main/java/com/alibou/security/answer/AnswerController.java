c85948d0572d72333146e859859b3e2c9a160760
Juhasen <juszczyk-krystian@wp.pl>
2025-05-27 20:16:35 +0200

    Added cross origin

--- src/main/java/com/alibou/security/answer/AnswerController.java
+++ src/main/java/com/alibou/security/answer/AnswerController.java
@@ -2,14 +2,11 @@ package com.alibou.security.answer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/answers")
 @RequiredArgsConstructor
