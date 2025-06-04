07f2a0f07ae2254a7d850117d5d91878ced88bed
Juhasen <juszczyk-krystian@wp.pl>
2025-06-04 11:36:25 +0200

    Update API version in QuestionController request mapping

--- src/main/java/com/alibou/security/question/QuestionController.java
+++ src/main/java/com/alibou/security/question/QuestionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/questions")
+@RequestMapping("/api/v1/questions")
 @RequiredArgsConstructor
 public class QuestionController {
 
