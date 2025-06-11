ace9d85f7f65e92d279eb71d4e11fbb7af338060
Juhasen <juszczyk-krystian@wp.pl>
2025-06-11 20:00:35 +0200

    Enhance quiz retrieval by incorporating user results to prioritize unanswered questions and shuffle answers

--- src/main/java/com/alibou/security/result/ResultRepository.java
+++ src/main/java/com/alibou/security/result/ResultRepository.java
@@ -1,14 +1,17 @@
 package com.alibou.security.result;
 
 import com.alibou.security.quiz.Quiz;
+import com.alibou.security.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResultRepository  extends JpaRepository<Result, Integer> {
+import java.util.List;
+
+public interface ResultRepository extends JpaRepository<Result, Integer> {
     Page<Result> findAllByUserId(Integer userId, Pageable pageable);
 
+    List<Result> findByUserAndQuiz(User user, Quiz quiz);
 
     void deleteAllByQuiz(Quiz quiz);
-
 }
